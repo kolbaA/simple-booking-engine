ab62c2f3790aa2dcdf7d6d3338917a6045a9ef08
kolba <kolbaonrails@gmail.com>
2021-06-04 21:05:02 +0300

    Fix extra end

--- app/queries/locations/available_rooms_query.rb
+++ app/queries/locations/available_rooms_query.rb
@@ -3,27 +3,25 @@
 module Locations
   class AvailableRoomsQuery
     def initialize(location:, from:, to:)
-      def initialize(location:, from:, to:)
-        @location = location
-        @from = from.to_date
-        @to = to.to_date
-      end
+      @location = location
+      @from = from.to_date
+      @to = to.to_date
+    end
 
-      def call
-        location.rooms.joins(:inventories).
-          where(inventories: { date: [from..to] }).
-          where('inventories.available_amount > 0').
-          group('rooms.id').
-          having('count(rooms.id) = ?', days_amount_in_daterange)
-      end
+    def call
+      location.rooms.joins(:inventories).
+        where(inventories: { date: [from..to] }).
+        where('inventories.available_amount > 0').
+        group('rooms.id').
+        having('count(rooms.id) = ?', days_amount_in_daterange)
+    end
 
-      private
+    private
 
-      attr_reader :from, :to, :location
+    attr_reader :from, :to, :location
 
-      def days_amount_in_daterange
-        (to - from).to_i + 1
-      end
+    def days_amount_in_daterange
+      (to - from).to_i + 1
     end
   end
 end
